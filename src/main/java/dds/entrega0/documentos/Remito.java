3791047e8061b2e2e03837c2f1fdcabe2e36a8e2
Alejandro Gonzalez <alex@sceu.frba.utn.edu.ar>
2020-04-13 00:13:52 -0300

    Agregando Remito a Documentos

---
+++ src/main/java/dds/entrega0/documentos/Remito.java
@@ -0,0 +1,14 @@
+package dds.entrega0.documentos;
+
+public class Remito extends DocumentoComercial {
+
+	int nro;
+
+	public int getNro() {
+		return nro;
+	}
+
+	public void setNro(int nro) {
+		this.nro = nro;
+	}
+}
