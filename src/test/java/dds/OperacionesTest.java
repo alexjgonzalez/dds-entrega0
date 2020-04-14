6e3e94754fb33ff2959aa0bfafebeb7fc4ac6e56
Alejandro Gonzalez <alex@sceu.frba.utn.edu.ar>
2020-04-14 00:59:51 -0300

    Test Operaciones

---
+++ src/test/java/dds/OperacionesTest.java
@@ -0,0 +1,17 @@
+package dds;
+
+import dds.entrega0.operaciones.Egreso;
+
+public class OperacionesTest {
+
+	public OperacionesTest() {
+		private Compra primerCompra;
+		private Articulo computadora = new Articulo("Computadora",100);
+		private Articulo escritorio = new Articulo("Escritorio",50);
+			
+		private Servicio mantenimiento = new Servicio("Mantenimiento",500);
+		
+		private Egreso egresoVacio = new Egreso();
+	}
+
+}
