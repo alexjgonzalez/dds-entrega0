cfc60e0f35f2273e7bc2441209be36a5f0725723
Alejandro Gonzalez <alex@sceu.frba.utn.edu.ar>
2020-04-14 20:00:47 -0300

    agrego Clase Exception Precio Final

---
+++ src/main/java/dds/exceptions/CambiarPrecioFinalException.java
@@ -0,0 +1,14 @@
+package dds.exceptions;
+
+@SuppressWarnings("serial")
+public class CambiarPrecioFinalException extends RuntimeException {
+
+	public CambiarPrecioFinalException() {
+		
+	}
+	
+	public CambiarPrecioFinalException(String arg0) {
+		super(arg0);
+	}
+	
+}
