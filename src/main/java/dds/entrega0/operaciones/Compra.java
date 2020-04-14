8d958470aa878d78880fa4e216e3e47c22dbb3d1
Alejandro Gonzalez <alex@sceu.frba.utn.edu.ar>
2020-04-14 02:20:20 -0300

    clase Compra en package operaciones

---
+++ src/main/java/dds/entrega0/operaciones/Compra.java
@@ -0,0 +1,16 @@
+package dds.entrega0.operaciones;
+
+public class Compra extends Egreso {
+
+	public Compra() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	
+	public void generarRemito() {
+		
+	}
+
+}
